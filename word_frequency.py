b964bd4e39d701c61e39bb90cd73074c3adc2c3d
Karthikeya Arikirevula <karikire@uoguelph.ca>
2025-10-20 13:32:49 -0400

    Fix typo in comment about splitting sentence
    Corrected a typo in the comment regarding splitting the sentence.

--- word_frequency.py
+++ word_frequency.py
@@ -3,7 +3,7 @@
 # Word frequency exercise
 # TODO: (Read detailed instructions in the Readme file)
 # 1. Prompt the user: Ask the user to enter a sentence.
-# 2. Split the sentence
+# 2. Split the sentence2
 # 3. Create lists to store words and their corresponding frequencies.
 # 4. Iterate through words and update frequencies
 
