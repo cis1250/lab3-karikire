9c81f5bc8ab5dab0ffa2d54da8c16291baa45364
Karthikeya Arikirevula <karikire@uoguelph.ca>
2025-10-20 12:35:57 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,6 +5,8 @@
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+
+#Here I get the user input and make sure its a whole number
 def get_num_terms():
     while True:
         user_input = input("How many terms of the Fibonacci sequence do you want?: ")
@@ -17,7 +19,7 @@ def get_num_terms():
             continue
         return k
 
-
+#The same code from before, to generate the fibonacci sequence
 def generate_fibonacci(n):
     sequence = []
     a, b = 0, 1
@@ -26,17 +28,16 @@ def generate_fibonacci(n):
         a, b = b, a + b
     return sequence
 
-
+#Makes each number a string because if it's integer the code will crash
 def print_sequence(seq):
     print("Fibonacci sequence:")
     print(" ".join(map(str, seq)))
 
-
+#The part that runs the actual program
 def main():
     num_terms = get_num_terms()
     fib_seq = generate_fibonacci(num_terms)
     print_sequence(fib_seq)
-
 
 if __name__ == "__main__":
     main()
